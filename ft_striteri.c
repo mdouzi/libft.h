6a17e4feda9016fe85a98307160b10c760f1340b
Med douzi <78613047+meddouzi@users.noreply.github.com>
2022-10-16 21:12:35 +0100

    Add files via upload

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,17 +6,23 @@
 /*   By: mdouzi <mdouzi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 20:56:19 by mdouzi            #+#    #+#             */
-/*   Updated: 2022/10/16 21:00:43 by mdouzi           ###   ########.fr       */
+/*   Updated: 2022/10/16 21:08:12 by mdouzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*))
+void ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-    int i;
-    char *str;
-    int len;
-    len = ft_strlen(s);
+   unsigned int i;
+   i  = 0;
 
+    if(!s)
+        return;
+
+    while (s[i])
+    {
+        f(i, &s[i]);
+        i++;
+    }
 }
--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ char *ft_strtrim(char const *s1, char const *set);
 char **ft_split(char const *s, char c);
 char *ft_itoa(int n);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 size_t ft_strlen(const char *s);
 size_t ft_strlcpy(char * dst, const char * src, size_t dstsize);
