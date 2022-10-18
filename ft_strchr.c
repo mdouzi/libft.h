e0cbcd5b3a18ae5a6572177c4a22fc66a2196654
Med douzi <78613047+meddouzi@users.noreply.github.com>
2022-10-18 23:37:18 +0100

    Add files via upload

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,20 +6,20 @@
 /*   By: mdouzi <mdouzi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 05:55:28 by mdouzi            #+#    #+#             */
-/*   Updated: 2022/10/09 21:44:40 by mdouzi           ###   ########.fr       */
+/*   Updated: 2022/10/18 23:32:34 by mdouzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char *ft_strchr(const char *s, int c)
+
+char	*ft_strchr(const char *s, int c)
 {
-    char *str;
-    str = (char *)s;
-    while (*str != c)
-    {
-        if (*str == '\0')
-            return (NULL);
-        str++;
-    }
-    return (str);
+	char	*str;
+
+	str = (char *)s;
+	while (*str && *str != (char)c)
+		str++;
+	if (*str == (char)c)
+		return (str);
+	return (0);
 }
